9c7600d3085b936b44c0b81d365a26f462f604d8
akash141 <akash@nimapinfotech.com>
2023-04-16 14:23:13 +0530

    added conversion from docx to pdf using aspose.word

---
+++ JobPortal.Api/Controllers/Helper/HelperController.cs
@@ -0,0 +1,39 @@
+﻿using Aspose.Words;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Api.Controllers.Helper
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelperController : BaseController
+    {
+
+        [HttpPost("ConvertToPdf")]
+        public IActionResult ConvertToPdf(IFormFile file)
+        {
+            var bytes = GetBytesFromFormFile(file);
+            MemoryStream ms = new MemoryStream(bytes);
+            Document doc = new Document(ms);
+            using (var pdfStream = new MemoryStream())
+            {
+                doc.Save(pdfStream, SaveFormat.Pdf);
+                return File(pdfStream.ToArray(), "application/pdf", "converted.pdf");
+            }
+        }
+
+        private byte[] GetBytesFromFormFile(IFormFile file)
+        {
+            using var memoryStream = new MemoryStream();
+            file.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+    }
+}
